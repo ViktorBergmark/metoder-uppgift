9a4455315cdda820d8f9402d4055818b4c1ecf66
Elev <Elev@TEB123012>
2019-10-21 12:02:06 +0200

    Metoder uppgift

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        
+        
+            public static void Main()
+            {
+                double radie = 2.4;
+                Console.WriteLine("Cirkelns omkrets: " + CirkelOmkrets(radie));
+                Console.WriteLine("Cirkelns area: " + CirkelArea(radie));
+
+            }
+
+            static double CirkelOmkrets(double radie)
+            {
+                return ((radie + radie) * 3.14);
+            }
+
+            static double CirkelArea(double radie)
+            {
+                return (radie * radie * 3.14);
+            }
+        }
+
+}
