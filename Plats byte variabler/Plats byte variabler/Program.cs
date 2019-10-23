41e1078cce37657e7560ff652efd18ca32b5991c
Elev <Elev@TEB123012>
2019-10-23 15:11:44 +0200

    Byta variabel

---
+++ Plats byte variabler/Plats byte variabler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Plats_byte_variabler
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int a = 10, b = 20;
+            BytPlats(ref a,ref b);
+            Console.WriteLine(a + " " + b);
+        }
+
+        public static void BytPlats(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+    }
+}
